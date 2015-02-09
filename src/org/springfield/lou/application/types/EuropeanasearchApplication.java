13f0a525ad457f096dcf781ef972abddcabd597f
OnWork <petar@noterik.nl>
2015-02-09 16:42:48 +0200

    Added albright filter drop down

--- src/org/springfield/lou/application/types/EuropeanasearchApplication.java
+++ src/org/springfield/lou/application/types/EuropeanasearchApplication.java
@@ -35,12 +35,69 @@ public class EuropeanasearchApplication extends Html5Application{
  	public EuropeanasearchApplication(String id) {
 		super(id); 
 	}
+ 	
+ 	public void searchfilter(Screen s,String content) {
+ 		System.out.println("searchFilter:" +content);
+ 		contentToProperties(s,content);
+ 		String filter = (String)s.getProperty("filtertype.value");
+ 		s.setProperty("filter", filter);
+ 		String url = (String)s.getProperty("videoid");
+ 		url = url + "/"+filter+"/";
+ 		
+ 		ServiceInterface albright = ServiceManager.getService("albright");
+		if (albright==null) {
+			String result = "Service Albright not found";
+			s.setContent("defaultoutput", result);
+		} else {
+			String result = albright.get(url,null,null);
+			if(result==null) { //No video
+				s.setContent("defaultoutput", "<h2>No such video</h2>");
+			} else { // Build up related result
+
+				System.out.println("Albright result: ");
+				System.out.println(result);
+				//  result contains the <fsxml> it must be parsed
+				FSList nodes = new FSList().parseNodes(result);
+	
+				//if there's a thumbnail property in the node you can make html and show it in defaultoutput
+				
+				System.out.println("NDOE SIZE DIVCODE="+nodes.size());
+				// Loop through all the nodes
+				// Available properties are :
+				/*
+				 * title
+				 * url
+				 * creator
+				 * thumbnail
+				 * provider
+				 */
+				String body = "<div>";
+				for(Iterator<FsNode> iter = nodes.getNodes().iterator() ; iter.hasNext(); ) {
+					FsNode n = (FsNode)iter.next(); 
+					body += "<div class='item'>";
+					String thumbnail = n.getProperty("thumbnail");
+					String objurl = n.getProperty("url");
+					
+					if(thumbnail!=null) { // Check if there is a thumbnail
+						body += "<a href='" + objurl + "' target=\"_blank\">";
+						body += "<img src='" + thumbnail + "' />";
+						body += "</a>";
+					}
+				   body += "</div>";
+				}
+				body += "</div>";
+				s.setContent("defaultoutput", body);
+			}
+		}
+ 	}
     
     public void searchvideo(Screen s,String content) {
-    	
+    	System.out.println("searchVideo:" +content);
 		contentToProperties(s,content);
 		String url = (String)s.getProperty("videouri.value");
 		System.out.println("VIDEO ID: " + url);
+		s.setProperty("videoid", url);
+		
 		url = url + "/ep_images/";
         
         ServiceInterface albright = ServiceManager.getService("albright");
@@ -57,7 +114,7 @@ public class EuropeanasearchApplication extends Html5Application{
 				//Build the video preview
 				String videobody ="<video id=\"video1\" controls preload=\"none\" data-setup=\"{}\">";
 
-				// if its a video we need its rawvideo node for where the file is.
+				// if it's a video we need it's rawvideo node for where the file is.
 				FsNode rawvideonode = Fs.getNode(videoPath+"/rawvideo/1");
 				if (rawvideonode!=null) {
 					String mounts[] = rawvideonode.getProperty("mount").split(",");
@@ -71,6 +128,21 @@ public class EuropeanasearchApplication extends Html5Application{
 					
 					s.setContent("player", videobody);
 				}
+				
+				FsNode videonode = Fs.getNode(videoPath);
+				if(videonode!=null) {
+					String genreProprty = videonode.getProperty("genre");
+					String terms = videonode.getProperty("ThesaurusTerm");
+					terms = terms.replaceAll(","," "); //Terms can be multiple separated by comma, so replace comma with space
+					String all = genreProprty + " " + terms;
+					
+					String body = "<input type=\"text\" value=\""+all+"\" />";
+					s.setContent("searchkeys", body);
+				}
+				
+				
+				
+				
 				System.out.println("Albright result: ");
 				System.out.println(result);
 				//  result contains the <fsxml> it must be parsed
